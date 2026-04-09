dc5522e55ea741aa24589cbbf78c496bd1bd80a8
Wake Liu <wake.gs@gmail.com>
2026-04-10 01:55:26 +0800

    fix: improve PaneSplitter visual feedback and hit area

--- spa/src/components/PaneSplitter.tsx
+++ spa/src/components/PaneSplitter.tsx
@@ -36,8 +36,25 @@ export function PaneSplitter({ direction, onResize }: Props) {
 
   return (
     <div
-      className={`shrink-0 ${direction === 'h' ? 'w-1 cursor-col-resize' : 'h-1 cursor-row-resize'} hover:bg-accent-base/30 active:bg-accent-base/50 transition-colors`}
+      className={`shrink-0 group relative ${
+        direction === 'h'
+          ? 'w-1 cursor-col-resize'
+          : 'h-1 cursor-row-resize'
+      }`}
       onMouseDown={handleMouseDown}
-    />
+    >
+      {/* Visible bar */}
+      <div className={`absolute ${
+        direction === 'h'
+          ? 'inset-y-0 left-1/2 -translate-x-1/2 w-[1px] group-hover:w-[3px] group-active:w-[3px]'
+          : 'inset-x-0 top-1/2 -translate-y-1/2 h-[1px] group-hover:h-[3px] group-active:h-[3px]'
+      } bg-border-subtle group-hover:bg-accent-base/50 group-active:bg-accent-base/70 transition-all`} />
+      {/* Invisible hit area */}
+      <div className={`absolute ${
+        direction === 'h'
+          ? 'inset-y-0 -left-1 -right-1'
+          : 'inset-x-0 -top-1 -bottom-1'
+      }`} />
+    </div>
   )
 }
